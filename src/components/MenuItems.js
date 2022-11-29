cf3272ac77ff3c1717291d7939b7f97dd5c2e860
rebeccahXYZ23 <115996332+rebeccahXYZ23@users.noreply.github.com>
2022-11-29 20:39:53 +0800

    Initial commit

---
+++ src/components/MenuItems.js
@@ -0,0 +1,27 @@
+export const MenuItems = [
+  {
+    title: "HOME",
+    url: "/",
+    cName: "nav-link"
+  },
+  {
+    title: "ABOUT",
+    url: "/about",
+    cName: "nav-link"
+  },
+  {
+    title: "SERVICES",
+    url: "/services",
+    cName: "nav-link"
+  },
+  {
+    title: "CONTACT",
+    url: "/contact",
+    cName: "nav-link"
+  },
+  {
+    title: "SIGN UP",
+    url: "/sign up",
+    cName: "nav-link mobile"
+  }
+];
---
+++ src/routes/Service.js
@@ -0,0 +1,11 @@
+import Navbar from "../components/Navbar";
+
+function Service() {
+  return (
+    <>
+      <Navbar />
+      <h1>About</h1>
+    </>
+  );
+}
+export default Service;
